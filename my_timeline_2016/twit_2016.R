440dc71e01a7e27347e26f3da308ba19a83eb49f
Hyun Gyo, Kim <khg423@gmail.com>
2017-01-01 23:26:20 +0900

    Update and rename MyWordCloud2016.R to twit_2016.R

--- my_timeline_2016/twit_2016.R
+++ my_timeline_2016/twit_2016.R
@@ -6,6 +6,7 @@ library(reshape2)
 library(wordcloud2)
 library(KoNLP)
 library(ggplot2)
+library(stringr)
 
 time_line <- read_csv(file.choose())
 time_line <- data.frame(time_line)
@@ -14,7 +15,9 @@ time_line <- time_line[!complete.cases(time_line$in_reply_to_status_id),]
 time_line$timestamp <- as.POSIXct(time_line$timestamp)
 
 time_line$sec <- as.numeric(time_line$timestamp)
-time_line_2016 <- subset(time_line, sec>=as.numeric(as.POSIXct('2016-01-01')))
+time_line_2016 <- subset(time_line, sec>=as.numeric(as.POSIXct('2016-01-01')) & sec<as.numeric(as.POSIXct('2017-01-01')))
+
+# wordcloud
 
 time_line_2016$text<- gsub("ㅋ", " ", time_line_2016$text)
 time_line_2016$text<- gsub("ㅎ", " ", time_line_2016$text)
@@ -39,3 +42,25 @@ text.noun.table<-table(text.noun$value)
 text.noun.table<-subset(data.frame(text.noun.table), Freq > 1)
 
 wordcloud2(data = text.noun.table, size = .5, fontFamily='NanumGothic', figPath = 'twit_pic.png',color = "skyblue",backgroundColor = "darkblue")
+
+# freq by day
+time_line_2016$day<-as.Date(time_line_2016$timestamp)
+time_line_2016_day<-time_line_2016 %>% group_by(day) %>% summarise(freq=n())
+ggplot(time_line_2016_day, aes(x= factor(day), y = freq)) + geom_bar(stat = "identity")
+write.table(time_line_2016_day, "time_line_2016_day.csv", quote = T, sep = ",", row.name=F)
+
+# freq by hour
+time_line_2016$hour<-as.POSIXlt(time_line_2016$timestamp)
+time_line_2016$hour<-time_line_2016$hour$hour+9
+time_line_2016_hour<-time_line_2016 %>% group_by(hour) %>% summarise(freq=n())
+time_line_2016_hour$hour<-with(time_line_2016_hour, ifelse(hour>=24, hour-24, hour))
+time_line_2016_hour <-rbind(time_line_2016_hour, data.frame(hour=c(5,6),freq=c(0,0)))
+ggplot(time_line_2016_hour, aes(x= factor(hour), y = freq)) + geom_bar(stat = "identity")
+
+# pic freq by day
+time_line_2016$pic<-ifelse(str_count(time_line_2016$expanded_urls, "photo|video")>0,1,0)
+time_line_2016$pic[is.na(time_line_2016$pic)]<-0
+time_line_2016_day.pic<-time_line_2016 %>% group_by(day) %>% summarise(freq=sum(pic))
+ggplot(time_line_2016_day.pic, aes(x= factor(day), y = freq)) + geom_bar(stat = "identity")
+time_line_2016_day.pic<-subset(time_line_2016_day.pic, freq>0)
+write.table(time_line_2016_day.pic, "time_line_2016_day.pic.csv", quote = T, sep = ",", row.name=F)
